e43af8c9ff6f35928e8be5b52335d9989469b153
Takayuki Maeda <takoyaki0316@gmail.com>
2025-09-07 02:09:13 +0900

    add

--- sarif/sarif.py
+++ sarif/sarif.py
@@ -9,3 +9,13 @@ def fibonacci(n):
         return 1
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
+
+def fibonacci_2(n):
+    """Compute the nth number in the Fibonacci sequence."""
+    x = 1
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
